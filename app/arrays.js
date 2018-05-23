4676635f7113a7454d06624008d37b78cf4a8a7e
Tibo Keri <tiborkeri@icloud.com>
2018-05-23 16:23:42 +0100

    tibranch

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,94 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+  indexOf: function (arr, item) {
+    return arr.indexOf(item);
+  },
+
+  sum: function (arr) {
+    var total = 0;
+    for (var i = 0; i < arr.length; i++) {
+      total += arr[i]
+    }
+    return total;
+  },
+
+  remove: function (arr, item) {
+    var uj = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== item) {
+        uj.push(arr[i]);
+      }
+    }
+    return uj;
+  },
+
+  removeWithoutCopy: function (arr, item) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1)
+        this.removeWithoutCopy(arr, item)
+      }
+    }
+    return arr
+  },
+
+  append: function (arr, item) {
+   arr.push(item);
+   return arr
+  },
+
+  truncate: function (arr) {
+     arr.pop()
+    return arr;
+  },
+
+  prepend: function (arr, item) {
+    arr.unshift(item);
+    return arr;
+  },
+
+  curtail: function (arr) {
+    arr.shift();
+    return arr;
+  },
+
+  concat: function (arr1, arr2) {
+   return  arr1.concat(arr2)
+  },
+
+  insert: function (arr, item, index) {
+      arr.splice(index, 0, item);
+    return arr;
+  },
+
+  count: function (arr, item) {
+    let occ = 0;
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i]===item){
+        occ++;
+      }
+    }
+    return occ;
 
   },
 
-  sum: function(arr) {
+  duplicates: function (arr) {
+    for(var i = 0 ; i < arr. length; i++){
+      if (arr.indexOf(i) !== arr.lastindexOf(i)) {
+        console.log(i)
+      }
+    }
+    
+   
 
   },
 
-  remove: function(arr, item) {
+  square: function (arr) {
 
   },
 
-  removeWithoutCopy: function(arr, item) {
-
-  },
-
-  append: function(arr, item) {
-
-  },
-
-  truncate: function(arr) {
-
-  },
-
-  prepend: function(arr, item) {
-
-  },
-
-  curtail: function(arr) {
-
-  },
-
-  concat: function(arr1, arr2) {
-
-  },
-
-  insert: function(arr, item, index) {
-
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
+  findAllOccurrences: function (arr, target) {
 
   }
 };
